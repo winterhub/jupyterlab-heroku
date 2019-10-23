31fd8d2d6a4626749f04aad32fd0fd18868440e3
winterhub <winterteaweiming@gmail.com>
2019-10-23 11:01:08 +0800

    add my passwd hash

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,3 +4,4 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'sha1:b7141b3ba9f9:980cab33b65a92a408d07382ea4cd8a3b93235e8'
